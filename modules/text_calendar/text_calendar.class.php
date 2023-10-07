91894b27c66058888c3dc7ed2bdb1921f7360ce4
Sergei Jeihala <sergejey@gmail.com>
2023-10-07 12:01:35 +0300

    Внешний календарь
    Поддержка добавления ссылок на внешние календари (Google и т.п.)

--- modules/text_calendar/text_calendar.class.php
+++ modules/text_calendar/text_calendar.class.php
@@ -6,6 +6,13 @@
  * @copyright http://majordomo.smartliving.ru/ (c)
  * @version 0.1 (wizard, 11:09:31 [Sep 30, 2023])
  */
+
+require_once DIR_MODULES . 'text_calendar/ICal/ICal.php';
+require_once DIR_MODULES . 'text_calendar/ICal/Event.php';
+
+use ICal\ICal;
+
+
 //
 //
 class text_calendar extends module
@@ -186,7 +193,6 @@ class text_calendar extends module
 
         if (!$tm) $tm = time();
 
-
         $week_days = array('Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб');
         $week_days_en = array(0 => "Sun", 1 => "Mon", 2 => "Tue", 3 => "Wed", 4 => "Thu", 5 => "Fri", 6 => "Sat");
 
@@ -196,6 +202,24 @@ class text_calendar extends module
             if (preg_match('/^\#/', $line) || trim($line) == '') {
                 continue;
             }
+
+            $line = trim($line);
+
+            if (preg_match('/^http/', $line)) {
+                list($url, $prefix) = explode(';', $line);
+                $external_events = $this->parseExternalCalendar($url, $tm);
+                foreach ($external_events as $item) {
+                    if ($prefix!='') {
+                        $matched[] = trim($prefix).' '.$item;
+                    } else {
+                        $matched[] = $item;
+                    }
+
+                }
+                continue;
+            }
+
+
             list($dt, $event) = explode(';', $line);
             $event = trim($event);
             if (!$event) continue;
@@ -288,6 +312,56 @@ class text_calendar extends module
         return $matched;
     }
 
+    function parseExternalCalendar($url, $tm = 0)
+    {
+        global $ical_cached;
+
+        if (!$tm) $tm = time();
+
+        $filename = ROOT . 'cms/cached/calendar_' . md5($url) . '.ics';
+        if (file_exists($filename)) {
+            $mtime = filemtime($filename);
+        } else {
+            $mtime = 0;
+        }
+
+        if ((time() - $mtime) > 15 * 60) {
+            $data = getURL($url);
+            if ($data != '') {
+                SaveFile($filename, $data);
+            }
+        }
+
+        if (!file_exists($filename)) return array();
+
+        $events = array();
+        try {
+            if (!isset($ical_cached[$url])) {
+                $ical_cached[$url] = new ICal($filename, array(
+                    'defaultSpan' => 2,     // Default value
+                    'defaultTimeZone' => 'UTC',
+                    'defaultWeekStart' => 'MO',  // Default value
+                    'disableCharacterReplacement' => true, // Default value
+                    'filterDaysAfter' => 180,  // Default value
+                    'filterDaysBefore' => 180,  // Default value
+                    'httpUserAgent' => null,  // Default value
+                    'skipRecurrence' => false, // Default value
+                ));
+            }
+            $result = $ical_cached[$url]->eventsFromRange(date('Y-m-d 00:00:00', $tm), date('Y-m-d 23:59:59', $tm));
+            if (isset($result[0])) {
+                foreach ($result as $item) {
+                    $events[] = $item->summary;
+                }
+            }
+        } catch (\Exception $e) {
+            return array();
+        }
+
+        return $events;
+
+    }
+
     /**
      * FrontEnd
      *
@@ -311,7 +385,7 @@ class text_calendar extends module
         if ($this->type == 'code') {
             $now = time();
             $events = $this->getEvents($this->calendar_id, $now);
-            $out['OUTPUT']='<pre>'.implode("\n",$events).'</pre>';
+            $out['OUTPUT'] = '<pre>' . implode("\n", $events) . '</pre>';
         }
 
         if ($this->type == 'week') {
@@ -358,7 +432,7 @@ class text_calendar extends module
 
         if ($this->type == 'today' || $this->type == 'tomorrow') {
             $tm = $this->type == 'today' ? time() : (time() + 24 * 60 * 60);
-            $out['BODY'] = '<div><big><b>' . date('d.m.Y') . ' (' . $week_days[date('w', $tm)] . ')</b></big></div>';
+            $out['BODY'] = '<div><big><b>' . date('d.m.Y', $tm) . ' (' . $week_days[date('w', $tm)] . ')</b></big></div>';
             if ($this->calendar_id) {
                 $calendar = SQLSelectOne("SELECT * FROM t_calendars WHERE ID=" . (int)$this->calendar_id);
                 $events = $this->getEventsForDay($calendar['CALENDAR'], $tm);
